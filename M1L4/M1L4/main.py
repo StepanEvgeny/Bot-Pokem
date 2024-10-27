7a8d423627cf7891ef790940f937dfd33e30dd30
StepanEvgeny <157963279+StepanEvgeny@users.noreply.github.com>
2024-10-27 11:23:33 +0100

    Update main.py

--- M1L4/M1L4/main.py
+++ M1L4/M1L4/main.py
@@ -31,5 +31,21 @@ def attack_pok(message):
 def start(message):
     bot.send_message(message.chat.id, "Привет! Я бот для игры в покемонов, скорее попробуй создать себе покемона, нажимай - /go")
 
+@bot.message_handler(commands='info')
+def information(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.info())
+        bot.send_photo(message.chat.id, pok.show_img())
+    else:
+        bot.reply_to(message,'Ты еще не создал покемона! Создать покемона сможешь через команду /go')
+@bot.message_handler(commands='feed')
+def feeding(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        poker = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id,poker.feed())
+    else:
+        bot.reply_to(message,'Ты еще не создал покемона! Создать покемона сможешь через команду /go')
+
 bot.infinity_polling(none_stop=True)
 
